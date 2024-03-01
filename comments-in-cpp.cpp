b7d0fe93a91b8b99e0ddd15ba955d6b33d15cd64
Beregszászi István <beistvan@gmail.com>
2024-03-01 21:24:38 +0100

    Comments In C++

---
+++ comments-in-cpp.cpp
@@ -0,0 +1,15 @@
+//Harry Potter
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Expecto Patronum\n";
+  
+  // Prints "hi!" to the terminal
+  // std::cout << "hi!";
+
+  /* This is all commented.
+  std::cout << "hi!";
+  None of this is going to run! */  
+}
