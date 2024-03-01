a856d88681794c45127866142cf164b5f4a96bb0
Beregszászi István <beistvan@gmail.com>
2024-03-01 21:18:17 +0100

    Hello World - Named C++ Executable

---
+++ hello_named.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Hello World!\n";
+  
+  /* Naming executable
+  Compile:
+  g++ hello_named.cpp -o hello_named
+
+  Run:
+  ./hello_named
+  */
+}
