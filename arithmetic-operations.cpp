cbf07b1e440a667c35047776fbe0709f779934d9
Beregszászi István <beistvan@gmail.com>
2024-03-12 11:13:18 +0100

    Arithmetic Operations

---
+++ arithmetic-operations.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+int main() {
+
+  int score = 0;
+
+  // Change score here:
+  score = 1234 * 99;
+  
+  std::cout << score << "\n";
+
+
+}
+
+/*
+int score = 0;
+// score is 0
+
+score = 4 + 2;
+// it is now 6
+
+score = 4 - 2;
+// it is now 2
+
+score = 4 * 2;
+// it is now 8
+
+score = 4 / 2;
+// and now 2
+
+score = 5 % 2;
+// and now 1
+*/
