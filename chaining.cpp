1c0a22a9766e90d8bf8da7a1998821a8db8ed3d7
Beregszászi István <beistvan@gmail.com>
2024-03-12 11:15:07 +0100

    Chaining

---
+++ chaining.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+int main() {
+  
+  int score = 0;
+  
+  // Output
+  std::cout << "Player score: " << score << "\n";
+   
+}
+
+/*
+int age = 28;
+
+std::cout << "Hello, I am ";
+std::cout << age;
+std::cout << " years old\n";
+// Hello, I am 28 years old
+*/
+
+/*
+int age = 28;
+
+std::cout << "Hello, I am " << age << " years old\n";
+*/
