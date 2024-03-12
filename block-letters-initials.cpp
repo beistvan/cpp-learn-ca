34a12ca983fbfa11920345c2521a4516a0661535
Beregszászi István <beistvan@gmail.com>
2024-03-12 11:06:30 +0100

    Block Letters Initials

---
+++ block-letters-initials.cpp
@@ -0,0 +1,13 @@
+// I like programming
+#include <iostream>
+ 
+int main() {
+ 
+  std::cout << "BBBB   IIIII\n";
+  std::cout << "B   B    I\n";
+  std::cout << "B   B    I\n";
+  std::cout << "BBBB     I\n";
+  std::cout << "B   B    I\n";
+  std::cout << "B   B    I\n";
+  std::cout << "BBBB   IIIII\n";
+}
