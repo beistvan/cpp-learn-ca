312ad6fb48e23da01970b129ca5cd8b579a95b9c
beistvan <beistvan@gmail.com>
2024-12-24 15:28:37 +0100

    References and Pointers - Review Cpp

---
+++ references-and-pointers-review.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() 
+{
+  
+  std::cout << "Dear Self,\n";
+  std::cout << "Build a VR game.\n";
+  std::cout << "3/1/2024, Vienna\n";
+  
+}
+// 24.Dec.2024., 42 Vienna
