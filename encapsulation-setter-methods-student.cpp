796d6bda5fb95a46cd676969e89eaef7a22f6bf5
beistvan <beistvan@gmail.com>
2024-12-28 14:28:49 +0100

    Update encapsulation-setter-methods-student.cpp

--- encapsulation-setter-methods-student.cpp
+++ encapsulation-setter-methods-student.cpp
@@ -8,6 +8,15 @@ int Car::getSpeed(){
   return speed;
 }
 
-void Car::setSpeed(int carSpeed) {
-  speed = carSpeed;
+int Car::setSpeed(int carSpeed) {
+  int returnCode;
+  if (carSpeed > 200) {
+    returnCode = 3; 
+  } else if (carSpeed < 0) {
+    returnCode = 2;
+  } else {
+    speed = carSpeed;
+    returnCode = 1; 
+  }
+  return returnCode;
 }
