b75e513eca4903e98816dcbe24d2553665e7182d
beistvan <beistvan@gmail.com>
2025-01-04 01:21:40 +0100

    Inheritance in C++ - Member Visibility In Inheritance - Vehicle Cpp

---
+++ inheritance-in-cpp-member-visibility-in-inheritance-vehicle.cpp
@@ -0,0 +1,11 @@
+#include "Vehicle.hpp"
+
+using namespace std;
+
+void Vehicle::setVehicleType(string vehicleType) {
+  type = vehicleType;
+}
+
+string Vehicle::getVehicleType() {
+  return type;
+}
