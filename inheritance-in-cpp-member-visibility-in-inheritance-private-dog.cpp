c1b1fb23b23b8544ef8cd3ce5d9ecfa2fa5aed9b
beistvan <beistvan@gmail.com>
2025-01-08 01:43:17 +0100

    Inheritance in C++ - Member Visibility In Inheritance - Private Dog Cpp

---
+++ inheritance-in-cpp-member-visibility-in-inheritance-private-dog.cpp
@@ -0,0 +1,14 @@
+#include "PrivateDog.hpp"
+using namespace std;
+
+void PrivateDog::setName(string dogName) {
+  name = dogName;
+}
+
+void PrivateDog::setSpecies(string dogSpecies) {
+  species = dogSpecies;
+}
+
+void PrivateDog::setAge(int dogAge) {
+  age = dogAge;
+}
