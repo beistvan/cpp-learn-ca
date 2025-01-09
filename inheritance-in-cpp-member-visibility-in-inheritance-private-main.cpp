bcd867d94c6cb857073354427a61018669fdbc6f
beistvan <beistvan@gmail.com>
2025-01-09 23:37:43 +0100

    Inheritance in C++ - Member Visibility In Inheritance - Main Cpp

---
+++ inheritance-in-cpp-member-visibility-in-inheritance-private-main.cpp
@@ -0,0 +1,13 @@
+#include "PrivateDog.hpp"
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  PrivateDog myDoggie;
+  myDoggie.species = "Cavachon";
+  myDoggie.name = "Milton";
+  myDoggie.age = 4;
+
+  return 0;
+}
